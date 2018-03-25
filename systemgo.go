9a8ebfb9e2234eaaa0f23d437afbe557606cb14d
bramz <brockramz@gmail.com>
2018-03-25 12:04:44 +0000

    Update watcher

--- systemgo.go
+++ systemgo.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	//	"github.com/howeyc/fsnotify"
+	"github.com/fsnotify/fsnotify"
 	"io/ioutil"
 	"os"
 	"os/exec"
+	//	"path/filepath"
 	"strconv"
 	"strings"
 	"syscall"
 	"time"
 )
+
+//var watcher *fsnotify.Watcher
 
 type Proc struct {
 	Name string
@@ -32,6 +37,27 @@ func (p *Proc) GetPid() int {
 	return int(p.Pid)
 }
 
+func watchProc(file string) error {
+	initStat, err := os.Stat(file)
+	if err != nil {
+		return err
+	}
+
+	for {
+		stat, err := os.Stat(file)
+		if err != nil {
+			return err
+		}
+
+		if stat.Size() != initStat.Size() || stat.ModTime() != initStat.ModTime() {
+			break
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+	return nil
+}
+
 func main() {
 	pn := os.Args[1]
 	use := os.Args[2]
@@ -48,7 +74,7 @@ func main() {
 		}
 		fmt.Println("Started", pn)
 
-		file, err := os.Create("pidfiles/" + filename + ".pid")
+		file, err := os.Create(".systemgo/pidfiles/" + filename + ".pid")
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -56,19 +82,76 @@ func main() {
 		pid := []byte(strconv.Itoa(cmd.Process.Pid))
 		file.Write(pid)
 
-		fpid, err := ioutil.ReadFile("pidfiles/" + filename + ".pid")
+		fpid, err := ioutil.ReadFile(".systemgo/pidfiles/" + filename + ".pid")
 		if err != nil {
 			fmt.Println(err)
 		}
 		fmt.Println("watching " + pn + " with pid: " + string(fpid))
 		fmt.Printf("%v", time.Now())
 		cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-        // uncomment for parent loop
-		//        for {
-		//            time.Sleep(time.Second)
-		//        }
+
+		/*
+			done := make(chan bool)
+			go func(done chan bool) {
+				defer func() {
+					done <- true
+				}()
+
+				watchfile := "~/systemgo/bin/" + filename
+				err := watchProc(watchfile)
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				fmt.Println("File has been changed")
+				fmt.Println(filename)
+			}(done)
+			<-done
+		*/
+
+		watcher, err := fsnotify.NewWatcher()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		done := make(chan bool)
+
+		// Process events
+		go func() {
+			for {
+				select {
+				case event := <-watcher.Events:
+					fmt.Println("event:", event)
+					if event.Op&fsnotify.Write == fsnotify.Write {
+						fmt.Println("modified file:", event.Name)
+						/*                        kill := exec.Command("TASKKILL", "/T", "/F", "/PID", strconv.Itoa(cmd.Process.Pid))
+						                          err = kill.Run()
+						                          if err != nil {
+						                              fmt.Println(err)
+						                          }
+						*/
+//						kill1 := exec.Command("kill", "-9", strconv.Itoa(cmd.Process.Pid))
+//						kill1.Start()
+//						kill2 := exec.Command("kill", "-9", strconv.Itoa(os.Getppid()))
+//						kill2.Start()
+					}
+				case err := <-watcher.Errors:
+					fmt.Println("error:", err)
+				}
+			}
+		}()
+
+		err = watcher.Add("bin")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// Hang so program doesn't exit
+        <-done
+
+		watcher.Close()
 	case "stop":
-		pid, err := ioutil.ReadFile("pidfiles/" + filename + ".pid")
+		pid, err := ioutil.ReadFile(".systemgo/pidfiles/" + filename + ".pid")
 		spid := string(pid)
 		out, err := exec.Command("kill", "-9", spid).CombinedOutput()
 		if err != nil {
