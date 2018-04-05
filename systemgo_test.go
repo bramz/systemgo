3952ae3ca87ba3954848e76821ee6288e900f447
bramz <brockramz@gmail.com>
2018-04-05 12:26:29 +0000

    Testing update

--- systemgo_test.go
+++ systemgo_test.go
@@ -1,32 +1,139 @@
-package main
+package systemgo
 
-import "testing"
+import (
+	"github.com/fsnotify/fsnotify"
+	"os"
+	"os/exec"
+	"testing"
+)
 
-type Task struct {
-	Self     string
-	Name     string
-	Status   string
-	Ppid     int
-	Pid      int
-	Filename string
-}
-
-func TestCli() {
+func TestCliStart(test *testing.T) {
 	t := &Task{
 		Self:     "systemgo",
 		Name:     "bin/httpd",
 		Status:   "start",
-		Ppid:     12345,
-		Pid:      12346,
+		Ppid:     "12345",
+		Pid:      "12346",
 		Filename: "httpd",
+	}
+
+	os.Args[0] = t.Self
+	os.Args[1] = t.Name
+	os.Args[2] = t.Status
+
+	if os.Args[0] == "systemgo" && os.Args[1] == "bin/httpd" && os.Args[2] == "start" {
+		TestStartTask(os.Args[1])
 	}
 }
 
-func TestStart() {
+func TestCliStop() {
+	t := &Task{
+		Self:     "systemgo",
+		Name:     "bin/httpd",
+		Status:   "stop",
+		Ppid:     "12345",
+		Pid:      "12346",
+		Filename: "httpd",
+	}
+
+	os.Args[0] = t.Self
+	os.Args[1] = t.Name
+	os.Args[2] = t.Status
+
+	if os.Args[0] == "systemgo" && os.Args[1] == "bin/httpd" && os.Args[2] == "stop" {
+		TestStopTask(os.Args[1])
+	}
+
 }
 
-func TestStop() {
+func TestCliRestart() {
+	t := &Task{
+		Self:     "systemgo",
+		Name:     "bin/httpd",
+		Status:   "restart",
+		Ppid:     "12345",
+		Pid:      "12346",
+		Filename: "httpd",
+	}
+
+	os.Args[0] = t.Self
+	os.Args[1] = t.Name
+	os.Args[2] = t.Status
+
+	if os.Args[0] == "systemgo" && os.Args[1] == "bin/httpd" && os.Args[2] == "restart" {
+		TestRestartTask(os.Args[1])
+	}
+
 }
 
-func TestRestart() {
+func TestStartTask(name string, filename string) {
+	cmd := exec.Command(name)
+	err := cmd.Start()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	file, err := os.Create(".systemgo/pidfiles/" + filename + ".pid")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	pid := []byte(strconv.Itoa(cmd.Process.Pid))
+	file.Write(pid)
+	fmt.Println("Started task", name)
+	TestwatchTask(cmd, name)
+	return
+
+}
+
+func TestWatchTask(cmd *exec.Cmd, name string) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	done := make(chan bool)
+
+	// handle events
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				fmt.Println("\nevent:", event)
+				if event.Op&fsnotify.Chmod == fsnotify.Chmod {
+					fmt.Println("Rebuild issued, restarting", cmd.Process.Pid)
+					kill := exec.Command("kill", "-9", strconv.Itoa(cmd.Process.Pid))
+					kill.Start()
+					cmd.Process.Kill()
+					cmd.Run()
+					run := exec.Command("./systemgo", name, "start")
+					run.Start()
+					os.Exit(1)
+				}
+			case err := <-watcher.Errors:
+				fmt.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add(name)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	<-done
+	watcher.Close()
+}
+
+func TestStopTask(name string) {
+    fmt.Println("Stopping task", name)
+    os.Exit(1)
+    return
+}
+
+func TestRestartTask(name string) {
 }
