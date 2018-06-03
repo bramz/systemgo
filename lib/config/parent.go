1b5bffff631f59d43d8dd55971e546777b96c33c
bramz <brockramz@gmail.com>
2018-06-03 09:42:30 +0000

    Parent config yaml skeleton

--- lib/config/parent.go
+++ lib/config/parent.go
@@ -1,27 +1,26 @@
 package config
 
 import (
-    "github.com/spf13/viper"
+    "fmt"
+    "io/ioutil"
     "log"
+    "gopkg.in/yaml.v2"
 )
 
-type ParentConfiguration struct {
-    name string
+type ParentConfig struct {
+    Parent struct {
+        Name string `yaml:name`
+        Root string `yaml:root`
+    } `yaml:"parent"`
 }
 
 func main() {
-    viper.SetConfigName("parent")
-    viper.AddConfigPath("confs")
-    var parentconf parent.Configuration
+    p := ParentConfig{}
 
-    if err := viper.ReadInConfig(); err != nil {
-        log.Fatalf("Error reading parent config, %s", err)
-    }
-
-    err := viper.Unmarshal(&parentconf)
+    yamlFile, err := ioutil.ReadFile("confs/parent.yaml")
     if err != nil {
-        log.Fatalf("Unable to decode into struct, %v", err)
+        log.Printf("yamlFile.Get err #%v ", err)
     }
-
-    log.Printf("name %s", parentconf.Parent.name)
+    err = yaml.Unmarshal(yamlFile, &p)
+    fmt.Printf("%+v\n", p)
 }
