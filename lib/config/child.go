5c43a80ed10a6d41106d6c5ecb525d36acf68a7e
bramz <brockramz@gmail.com>
2018-06-06 04:18:15 +0000

    Added keys, fetch and load functions

--- lib/config/child.go
+++ lib/config/child.go
@@ -1,26 +1,43 @@
 package config
 
 import (
-    "fmt"
+    "os"
     "io/ioutil"
+	"gopkg.in/yaml.v2"
     "log"
-    "gopkg.in/yaml.v2"
+    "fmt"
 )
 
-type ChildConfig struct {
-    Child struct {
-        Name string `yaml:name`
-        Bin string `yaml:bin`
-    } `yaml:"child"`
+type Child struct {
+	Name  string  `yaml:name`
+	Bin   string  `yaml:bin`
+	Tasks []*Task `yaml:"child"`
 }
 
-func main() {
-    c := ChildConfig{}
+func (c Child) Keys() []string {
+    keys := []string()
+    for _; t := range c.Tasks {
+        keys = append(keys, t.Name)
+    }
+    return keys
+}
 
-    yamlFile, err := ioutil.ReadFile("confs/child.yaml")
+func (c Child) Fetch(key string) *Task {
+    for _, t := range c.Tasks {
+        if t.Name == key {
+            return t
+        }
+    }
+    return nil
+}
+
+func LoadChildConfig(c Child) {
+    yamlFile, err := ioutil.Readfile("confs/child.yaml")
+
     if err != nil {
         log.Printf("yamlFile.Get err #%v ", err)
     }
-    err = yaml.Unmarshal(yamlFile, &p)
-    fmt.Printf("%+v\n", p)
+    err = yaml.Unmarshal(yamlFile, &c)
+    fmt.Printf("%+v\n", c)
+    return c
 }
