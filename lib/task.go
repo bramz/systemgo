4aabbdfde29bf089ca249aaeeb214f09c2cd736e
bramz <brockramz@gmail.com>
2018-06-15 23:43:53 +0000

    Add watchState, update StopTask

--- lib/task.go
+++ lib/task.go
@@ -1,112 +1,130 @@
 package lib
 
 import (
-    "fmt"
-    "github.com/fsnotify/fsnotify"
-    "io/ioutil"
-    "os"
-    "os/exec"
-    "strconv"
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strconv"
 )
 
 type Task struct {
-    Self     string
-    Name     string
-    State   string
-    Ppid     int
-    Pid      int
-    Filename string
-    Cmd      string
+	Self     string
+	Name     string
+	State    string
+	Ppid     int
+	Pid      int
+	Filename string
+	Proc     *os.Process
+	Cmd      *exec.Cmd
 }
 
-func StartTask(name string, filename string) {
-    cmd := exec.Command(name)
-    err := cmd.Start()
+func StartTask(t *Task) {
+	cmd := exec.Command(t.Name)
+	err := cmd.Start()
 
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    file, err := os.Create(".systemgo/pidfiles/" + filename + ".pid")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    defer file.Close()
+	file, err := os.Create(".systemgo/pidfiles/" + t.Filename + ".pid")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
 
-    pid := []byte(strconv.Itoa(cmd.Process.Pid))
-    file.Write(pid)
-    fmt.Println("Started task", name)
-    watchTask(cmd, name)
-    return
+	pid := []byte(strconv.Itoa(cmd.Process.Pid))
+	file.Write(pid)
+	t.State = "running"
+	watchTask(cmd, t.Name, t)
+	return
 }
 
+func watchTask(cmd *exec.Cmd, name string, t *Task) {
+	watchState(t)
 
-func watchTask(cmd *exec.Cmd, name string) {
-    watcher, err := fsnotify.NewWatcher()
-    if err != nil {
-        fmt.Println(err)
-    }
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    done := make(chan bool)
+	done := make(chan bool)
 
-    // handle events
-    go func() {
-        for {
-            select {
-            case event := <-watcher.Events:
-                fmt.Println("\nevent:", event)
-                if event.Op&fsnotify.Chmod == fsnotify.Chmod {
-                    fmt.Println("Rebuild issued, restarting", cmd.Process.Pid)
-                    kill := exec.Command("kill", "-9", strconv.Itoa(cmd.Process.Pid))
-                    kill.Start()
-                    cmd.Process.Kill()
-                    cmd.Run()
-                    run := exec.Command("./systemgo", name, "start")
-                    run.Start()
-                    os.Exit(1)
-                }
-            case err := <-watcher.Errors:
-                fmt.Println("error:", err)
-            }
-        }
-    }()
+	// watch for file change events
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				fmt.Println("\nevent:", event)
+				if event.Op&fsnotify.Chmod == fsnotify.Chmod {
+					fmt.Println("Rebuild issued, restarting", cmd.Process.Pid)
+					kill := exec.Command("kill", "-9", strconv.Itoa(cmd.Process.Pid))
+					kill.Start()
+					cmd.Process.Kill()
+					cmd.Run()
+					run := exec.Command("./systemgo", name, "start")
+					run.Start()
+					os.Exit(1)
+				}
+			case err := <-watcher.Errors:
+				fmt.Println("error:", err)
+			}
+		}
+	}()
 
-    err = watcher.Add(name)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    <-done
-    watcher.Close()
+	err = watcher.Add(name)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	<-done
+	watcher.Close()
+
 }
 
-func StopTask(name string) {
-    fmt.Println("Stopping task", name)
-    pid, err := ioutil.ReadFile(".systemgo/pidfiles/" + name + ".pid")
-    spid := string(pid)
-    out, err := exec.Command("kill", "-9", spid).CombinedOutput()
-    if err != nil {
-        fmt.Println(err)
-        return
-    } else {
-        fmt.Println("Stopping", name, out)
-//      out.Run()
-        return
-    }
-    return
+func watchState(t *Task) {
+	cstate := make(chan *os.ProcessState)
+
+	go func() {
+		state, err := t.Proc.Wait()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		cstate <- state
+	}()
+	select {
+	case s := <-state:
+		if t.State == "stopped" {
+			fmt.Println("Process is not running")
+			return
+		}
+	}
 }
 
-func RestartTask(pid int, name string) {
-    fmt.Println("Restarting task", name)
-    kill := exec.Command("kill", "-9", strconv.Itoa(pid))
-    kill.Start()
-    kill.Process.Kill()
-    kill.Run()
-    run := exec.Command("./systemgo", name, "start")
-    run.Start()
-    os.Exit(1)
-    return
+func StopTask(t *Task) {
+	fmt.Println("Stopping task")
+	pid, err := ioutil.ReadFile(".systemgo/pidfiles/" + t.Filename + ".pid")
+	if err != nil {
+		fmt.Println(err)
+	}
+	out := exec.Command("kill", "-9", string(pid))
+	out.Run()
+	t.State = "stopped"
+	return
 }
 
+func RestartTask(pid int, t *Task) {
+	fmt.Println("Restarting task")
+	kill := exec.Command("kill", "-9", strconv.Itoa(pid))
+	kill.Start()
+	kill.Process.Kill()
+	kill.Run()
+	run := exec.Command("./systemgo", t.Name, "start")
+	run.Start()
+	os.Exit(1)
+	return
+}
